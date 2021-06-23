91a50a94d621cbb405f1ca34bf2ad30cd9801f58
kahkuin <85871220+kahkuin@users.noreply.github.com>
2021-06-24 03:03:25 +0800

    Add files via upload

---
+++ information.php
@@ -0,0 +1,192 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Information Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+    table, th, td{
+        border: 1px solid black; 
+        border-collapse: collapse;
+    }
+    th, td{
+        padding: 15 px;
+        text-align: left;
+        }
+    tr {
+    background-color: orange;}  
+
+        body{ font: 16px sans-serif; padding: 30px; text-align: left; }
+    </style>
+</head>
+<body>
+    <h1> <strong> Project Complexity and Risk Assessment Tool </strong> </h1>
+     <em> About: </em>
+     <p> The Project Complexity and Risk Assessment Tool (PCRA) is intended to support the Treasury Board Policy on the Management of Projects and the Standard for Project Complexity and Risk. </p>
+     <p> The Treasury Board Policy on the Management of Projects requires deputy heads to ensure that each planned or proposed project which is subject to the Policy is accurately assessed to determine its level of risk and complexity for the purposes of project approval and expenditure authority </p>
+     <p> This questionnaire is a derivative of Interis Consulting Inc.'s taxonomy-based questionnaire for project risk assessments. Interis' questionnaire draws extensively on the Continuous Risk Management Guidebook (1999) of the Software Engineering Institute (SEI). In consultation with project managers in the Government of Canada Interis has modified and extended the series of questions to reflect public sector and broader strategic considerations. </p>
+    <p> </p>
+    <p> <strong> Risk and Complexity Definitions </strong> </p>
+    <p> The SEI, the authoring agency of the Continuous Risk Management Guidebook, uses the following definition for the term "risk":
+The possibility of suffering loss. - <em> Source: Webster's </em> </p>
+    <p> The Government of Canada (GC) cites the SEI as the basis for the concepts, methods, and guidelines embodied in the Integrated Risk Management Framework (IRMF). Within that Framework, the following definition of risk provides a standard for the GC: 
+    <P> "<strong>Risk</strong> refers to the uncertainty that surrounds future events and outcomes. It is the expression of the likelihood and impact of an event with the potential to influence the achievement of an organization's objectives." </P> <em> Source: Government of Canada: Integrated Risk Management Framework (2001) </em> </p>
+    <p> </p>
+    <p><strong> Complexity : </strong></p>
+    <li>(Apparent) the degree to which a system or component has a design or implementation that is difficult to understand and verify; </li>
+    <li> (Inherent) the degree of complication of a system or system component, determined by such factors as the number and intricacy of interfaces, the number and intricacy of conditional branches, the degree of nesting, and the types of data structures. </li>
+    <p> </p>
+
+    <h5><strong> Instruction: </strong></h5> 
+    <p> The PCRA contains 64 questions. The questions are all given an equal percentage in the overall score. This tool is accompanied by the PCRA User Guide and an Excel spreadsheet that will tabulate the final score and rating automatically. </p>
+    <p> There are a few rules for completing the PCRA: </p>
+    <li>Every question must be answered. If you are sure a question does not apply to your project, answer with the lowest score ("1") for that question; </li>
+    <li>If the answer to a question is unknown, answer with the highest score ("5") for that question; and </li>
+    <li>If you answer "1" to Question 2 in the "Project characteristics" section (3.1), questions in the "Procurement risks" section (3.3) should be answered with a "1" only. </li>
+    <p> </p>
+    <p> The tool is also available on-line. For further information on how to setup a user account and complete the assessment online, please contact your departmental coordinator or agency lead. </p>
+
+    <h5><strong> Description of the sections </strong></h5> 
+    <p>The assessment is divided into seven sections or categories of questions. They are described in table below. </h4>
+   
+    <table style="width:90%">
+    <tr>
+    <th style ="width:30%">Section</th>
+    <th>Description</th>
+    </tr>
+
+    <tr>
+    <td>Project Characteristics (18 Questions)</td>
+    <td>This category is designed to build a profile of a given project with respect to key attributes, including funding, budget, size and number of resources, duration, scope, technology scope, stakeholders, dependencies, and external considerations.</td> 
+     </tr>
+
+     <tr>
+    <td>Strategic Management Risks (6 Questions)</td>
+    <td>This category assesses a project's alignment with the organization's investment plan:
+     <ul><li> Is the project well-positioned to achieve the goals and objectives of the plan? </li>
+     <li>Is the project a potential risk to the plan? </li></ul></td>
+     </tr>
+
+    <tr>
+    <td>Procurement Risks (9 Questions)</td>
+    <td>This category assesses the extent to which procurement activities present potential risks to the project. </td> 
+    </tr>
+
+    <tr>
+    <td>Human Resource Risks (5 Questions)</td>
+    <td>This category assesses the extent to which procurement activities present potential risks to the project. </td>
+    </tr>
+    
+    <tr>
+    <td>Business Risks (5 Questions)</td>
+    <td>This category assesses the extent to which the project affects the organization operationally and from a legislative perspective. </td>
+    </tr>
+    
+
+    <tr>
+    <td>Project Management Integration Risks (6 Questions)</td>
+    <td>This category assesses whether the project demonstrates implementation of key project management control measures and deliverables. This assessment includes addressing the state of the project management plan, project management and control disciplines, and information management processes. </td>
+    </tr>
+
+    <tr>
+    <td>Project Requirements Risks (15 Questions)</td>
+    <td>This category assesses, by considering the number, type, and degree of certainty of the requirements, the extent to which the specific requirements of the project add risk and complexity. </td>
+    </tr>
+    </table>
+    <p> </p>
+<h5><strong> Table 2 - Value of the sections </strong></h5>
+<table style="width:50%"; >
+    <tr>
+    <th style ="width:50%">Section</th>
+    <th>Number of Questions</th>
+    <th>Maximum Score</th>
+    </tr>
+
+    <tr>
+    <td>Project Characteristics</td>
+    <td> 18 </td>
+    <td>90</td>
+    </tr>
+
+    <tr>
+    <td>Strategic Management Risks</td>
+    <td> 6 </td>
+    <td>30</td>
+    </tr>
+
+    <tr>
+    <td>Procurement Risks</td>
+    <td> 9 </td>
+    <td>45</td>
+    </tr>
+    
+    <tr>
+    <td>Human Resource Risks</td>
+    <td> 5 </td>
+    <td>25</td>
+    </tr>
+
+    <tr>
+    <td>Business Risks</td>
+    <td> 5 </td>
+    <td>25</td>
+    </tr>
+
+    <tr>
+    <td>Project Management Integration Risks</td>
+    <td> 6 </td>
+    <td>30</td>
+    </tr>
+
+    <tr>
+    <td>Project Requirements Risks</td>
+    <td> 15 </td>
+    <td>75</td>
+    </tr>
+
+    <tr>
+    <td>Total</td>
+    <td> 64 </td>
+    <td>320</td>
+    </tr>
+</table>
+<p> </p>
+    <p> The questions are all of equal value in the overall score. Please note though that if questions 1, 3, and 11, which deal with money, scope, and time in the project characteristics section, are all answered as '5', a triple constraint condition will apply resulting in '5' response scoring for all questions in this section (i.e. the maximum score of 90 for the section). In addition, if the project has no procurement (addressed in question 2) the minimum score is automatically assigned for each question in the procurement section. </p>
+    <p> The criteria in the PCRA consider a very broad range of potential project risks which stem from virtually every possible root cause relevant for just about any project. However, not every project risk will apply to every project in every instance. When the PCRA was validated in 2009, it was determined that approximately 70% of the project risks reflected in the assessment criteria would apply to any single project. Therefore, when calculating the final PCRA score, the total numeric value is normalized to accurately reflect the more realistic range of relevant risks for a single project. </p>
+    <p> </p>
+    
+    <h5><strong> Table 3 - Complexity and Risk Level Defined </strong></h5>
+<table style="width:90%"; >
+    <tr>
+    <th style ="width:20%">Complexity and Risk Level</th>
+    <th style ="width:55%">Definition</th>
+    <th>Score</th>
+    </tr>
+
+    <tr>
+    <td><strong>1. Sustaining </strong></td>
+    <td> Project has low risk and complexity. The project outcome affects only a specific service or at most a specific program, and risk mitigations for general project risks are in place. The project does not consume a significant percentage of departmental or agency resources. </td>
+    <td> less than 45</td>
+    </tr>
+
+    <tr>
+    <td><strong>2. Tactical </strong></td>
+    <td> A project rated at this level affects multiple services within a program and may involve more significant procurement activities. It may involve some information management or information technology (IM/IT) or engineering activities. The project risk profile may indicate that some risks could have serious impacts, requiring carefully planned responses. The scope of a tactical project is operational in nature and delivers new capabilities within limits.</td>
+    <td>45 to 63</td>
+    </tr>
+
+    <tr>
+    <td><strong>3. Evolutionary </strong></td>
+    <td>As indicated by the name, projects within this level of complexity and risk introduce change, new capabilities and may have a fairly extensive scope. Disciplined skills are required to successfully manage evolutionary projects. Scope frequently spans programs and may affect one or two other departments or agencies. There may be substantial change to business process, internal staff, external clients, and technology infrastructure. IM/IT components may represent a significant proportion of total project activity. </td>
+    <td>64 to 82</td>
+    </tr>
+    
+    <tr>
+    <td><strong>4. Transformational </strong></td>
+    <td> At this level, projects require extensive capabilities and may have a dramatic impact on the organization and potentially other organizations. Horizontal (i.e. multi-departmental, multi-agency, or multi-jurisdictional) projects are transformational in nature. Risks associated with these projects often have serious consequences, such as restructuring the organization, change in senior management, and/or loss of public reputation. </td>
+    <td>83 and over</td>
+    </tr>
+</table>
+</body>
+</html>
